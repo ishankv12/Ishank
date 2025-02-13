3ddd398d63de99c7921a1aea4f13f1355d16b3ff
ishankv12 <ishankv34@gmail.com>
2025-02-13 12:06:02 +0530

    proj-1

---
+++ project-1.py
@@ -0,0 +1,13 @@
+def main():
+    n1=float(input('enter first number:'))
+    n2=float(input('enter second number:'))
+    add=n1+n2
+    sub=n1-n2
+    mul=n1*n2
+    div=n1/n2
+    print('n1+n2:',add)
+    print('n1-n2:',sub)
+    print('n1*n2;',mul)
+    print('n1/n2:',div)
+if __name__=='__main__':
+    main()
