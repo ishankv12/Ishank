2e10c0c29f4479c6f99ab3997e35627733916210
ishankv12 <ishankv34@gmail.com>
2025-02-27 11:34:10 +0530

    add.c

--- add.c
+++ add.c
@@ -4,7 +4,6 @@ void main()
 	int num1,num2;
 	scanf("%d",&num1,&num2);
 	printf("Addition of 2 Numbers:%d",num1+num2);
-	printf("subtraction of 2 Numbers:%d",num1-num2);
-	printf("multiplication of 2 Numbers:%d",num1*num2);
-	printf("Division of 2 Numbers:%d",num1/num2);
+	printf("Subtraction:%d",num1-num2);
+	printf("Multiplication:%d",num1*num2);
 }
