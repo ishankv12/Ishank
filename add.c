8dc1bf0d068f722744745c60b0683c64afd57532
ishankv12 <ishankv34@gmail.com>
2025-03-06 11:28:54 +0530

    Added sum of a+b to add.c

--- add.c
+++ add.c
@@ -2,6 +2,8 @@
 void main()
 {
 	int num1,num2;
+	int a=10,b=10;
+	printf("sum:%d",a+b);
 	scanf("%d",&num1,&num2);
 	printf("Addition of 2 Numbers:%d",num1+num2);
 	printf("Subtraction:%d",num1-num2);
